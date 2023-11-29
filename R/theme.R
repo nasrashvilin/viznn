a89b9326b38a6f537e5efe1cbe98defda5af6ebf
Nika Nasrashvili <nikanasra@gmail.com>
2023-11-29 13:20:02 +0400

    Add files via upload
    some theme elements

---
+++ R/theme.R
@@ -0,0 +1,68 @@
+# theme
+
+#set theme for charts
+theme_ndi_ge <- function() {
+  library(ggplot2)
+
+  theme_minimal() +
+    theme(
+      # grids
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+
+      # axis
+      axis.title = element_blank(),
+      axis.text.y = element_text(color='black', size=12),
+      axis.text.x = element_blank(),
+
+      # plot title
+      plot.title.position = 'plot',
+      plot.title = element_text(size=20, color='#404040', face = 'bold'),
+
+
+      text = element_text(family = 'BPG Nino Mtavruli', color='black'),
+      # strips
+      strip.text = element_text(size=12, color='black', vjust=1),
+      panel.spacing = unit(.8, 'lines'),
+      panel.spacing.y = unit(.6, 'lines'),
+
+
+      plot.margin = margin(t=.5, r=0.5,b=0.5, l=0.5, unit='cm'),
+      plot.background = element_rect(color=NA, fill=NA),
+
+    )
+}
+
+
+# Georgian theme
+
+# basic theme for charts
+
+theme_nn <- function() {
+
+  library(ggplot2)
+
+
+  theme_minimal()+
+
+    theme(
+      #title and subtitle
+      plot.title.position = 'plot',
+      plot.title = element_text(hjust=0, size=18, vjust=0),
+      plot.subtitle = element_text(hjust=0, size=9, vjust=1),
+
+      ## axis
+      axis.text.y = element_text(size=10, hjust=1),
+      axis.title = element_blank(),
+
+      #margins
+      plot.margin = margin(t=.5,r=.5,b=.5,l=.5, 'cm'),
+
+      # gridlines
+      panel.grid.minor = element_blank(),
+      panel.grid.major.x = element_blank(),
+
+      #legend
+      legend.position = 'top'
+    )
+}
