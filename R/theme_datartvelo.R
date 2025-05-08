11090715dd175f40c20dce283e1198543c16aab9
Nika Nasrashvili <nikanasra@gmail.com>
2025-05-08 20:11:30 +0400

    update_theme elements

--- R/theme_datartvelo.R
+++ R/theme_datartvelo.R
@@ -1,5 +1,5 @@
 theme_datartvelo_en <- function () {
-  theme_minimal()+
+  ggplto2::theme_minimal()+
     theme(
 
       
@@ -9,8 +9,10 @@ theme_datartvelo_en <- function () {
                            b=1.5, 
                            l=1.5, 
                            unit="cm"),
-      plot.background = element_rect(fill='white',
-                                     color=NA),
+
+      plot.background = element_rect(
+        fill='white',
+        color=NA),
 
       # plot title 
       plot.title = element_text(
@@ -41,11 +43,13 @@ theme_datartvelo_en <- function () {
 
       
       #axis 
-      axis.line.x = element_line(linewidth = .8),
+      axis.line.x = element_line(linewidth = .5),
       axis.ticks.x = element_line(),
-      axis.ticks.length.x = unit(1, 'line'),
+      axis.ticks.length.x = unit(.7, 'line'),
       axis.title = element_blank(),
-      axis.text.y = element_text(size=12, color='black'),
+      axis.text.y = element_text(
+        size=12, 
+        color='black'),
       axis.text.y.left = element_text(color='grey50',
                                  size=12,
                                  vjust=-.5,
@@ -84,40 +88,75 @@ theme_datartvelo_en <- function () {
       # panel grids
       panel.grid.minor.x = element_blank(),
       panel.grid.major.x = element_blank(), 
-      panel.grid.major.y = element_line(linewidth = .4, color = 'grey'),
+      panel.grid.major.y = element_line(
+        linewidth = .5, 
+        color = 'grey'),
       panel.grid.minor.y = element_blank()
     )
 }
 
 ### Georgian 
-theme_datartvelo_ge <- function () {
-  bbplot::bbc_style()+
+theme_datartvelo_ge <- <- function () {
+  ggplto2::theme_minimal()+
     theme(
-      plot.margin = margin(t=1, r=1.5, b=.5, l=1, unit="cm"),
-      plot.title = element_text(size=30, #face="bold",  
-                                family = 'GL Gordeziani',
-                                vjust=0),
-      plot.subtitle = element_text(size = 15,
-                                   family = 'BPG Arial',),
-      plot.caption = element_text(size=12, color='black',
-                                  family = 'BPG Arial',
-                                  hjust = 0),
-      plot.background = element_rect(fill='white',
-                                     color=NA),
-      axis.line.x = element_line(linewidth = .8),
+
+      
+      # set plot margin 
+      plot.margin = margin(t=1.5, 
+                           r=1.5, 
+                           b=1.5, 
+                           l=1.5, 
+                           unit="cm"),
+
+      plot.background = element_rect(
+        fill='white',
+        color=NA),
+
+      # plot title 
+      plot.title = element_text(
+        size = 18,
+        vjust=0,
+        family = 'BPG Nino Mtavruli',
+        face = 'bold',
+        lineheight = .9),
+      
+      plot.title.position = 'plot',
+      
+      # plot subtitle
+      plot.subtitle = element_text(
+        size=11,
+        family = 'BPG Arial',
+        color='grey20',
+        vjust=-2,
+        lineheight = 1),
+      
+      # plot caption
+      plot.caption.position = 'plot',
+      
+      plot.caption = element_text(
+        size=11, 
+        hjust=0,
+        family = 'BPG Arial',
+        color='grey50'),
+
+      
+      #axis 
+      axis.line.x = element_line(linewidth = .5),
       axis.ticks.x = element_line(),
-      axis.ticks.length.x = unit(1, 'line'),
+      axis.ticks.length.x = unit(.7, 'line'),
       axis.title = element_blank(),
-      axis.text.y = element_text(size=15, color='black'),
+      axis.text.y = element_text(
+        size=12, 
+        color='black'),
       axis.text.y.left = element_text(color='grey50',
-                                 size=14,
+                                 size=12,
                                  vjust=-.5,
                                  margin = margin(t=0,
                                                  b=0,
                                                  r=-12,
                                                  l=0,
                                                  'pt')),
-      axis.text.x = element_text(size=15,
+      axis.text.x = element_text(size=13,
                                  hjust=.5,
                                  vjust=-1,
                                  margin = margin(t=0,
@@ -125,6 +164,31 @@ theme_datartvelo_ge <- function () {
                                                  b=50,
                                                  l=0,
                                                  'pt')),
-      legend.position = 'none'
+      
+      axis.text.y.right = element_text(
+        margin = margin(
+          t= 0,
+          r= 0,
+          b= 0,
+          l=-22,
+          'pt'),
+        vjust=-.5,
+        hjust = 1,
+        color='grey',
+        family = 'Source Sans Pro',
+      size = 12),
+
+
+      
+      # Legend
+      legend.position = 'none',
+      
+      # panel grids
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_blank(), 
+      panel.grid.major.y = element_line(
+        linewidth = .5, 
+        color = 'grey'),
+      panel.grid.minor.y = element_blank()
     )
 }
