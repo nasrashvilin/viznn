16a7181424f57e7bc30bfeece7aef94bb07a237a
Nika Nasrashvili <nikanasra@gmail.com>
2025-05-08 19:54:34 +0400

    Update theme_datartvelo.R

--- R/theme_datartvelo.R
+++ R/theme_datartvelo.R
@@ -1,20 +1,45 @@
-theme_datartvelo <- function () {
+theme_datartvelo_en <- function () {
   theme_minimal()+
     theme(
+
+      
       # set plot margin 
-      plot.margin = margin(t=1, r=1.5, b=.5, l=1, unit="cm"),
+      plot.margin = margin(t=1.5, 
+                           r=1.5, 
+                           b=1.5, 
+                           l=1.5, 
+                           unit="cm"),
       plot.background = element_rect(fill='white',
                                      color=NA),
-      # Title 
-      plot.title = element_text(size=25, face="bold",  
-                                family = 'Source Sans Pro',vjust=0),
+
+      # plot title 
+      plot.title = element_text(
+        size = 20,
+        vjust=0,
+        family = 'Source Sans Pro',
+        face = 'bold',
+        lineheight = .9),
+      
       plot.title.position = 'plot',
-      #subtitle
-      plot.subtitle = element_text(size = 13,
-                                   family = 'Source Sans Pro',),
-      # Caption
+      
+      # plot subtitle
+      plot.subtitle = element_text(
+        size=12,
+        family = 'Source Sans Pro',
+        color='grey20',
+        vjust=-2,
+        lineheight = 1),
+      
+      # plot caption
       plot.caption.position = 'plot',
-      plot.caption = element_text(size=12, color='black', hjust = 0,family = 'Calibri',),
+      
+      plot.caption = element_text(
+        size=11, 
+        hjust=0,
+        family = 'Source Sans Pro',
+        color='grey50'),
+
+      
       #axis 
       axis.line.x = element_line(linewidth = .8),
       axis.ticks.x = element_line(),
@@ -37,13 +62,29 @@ theme_datartvelo <- function () {
                                                  b=50,
                                                  l=0,
                                                  'pt')),
+      
+      axis.text.y.right = element_text(
+        margin = margin(
+          t= 0,
+          r= 0,
+          b= 0,
+          l=-22,
+          'pt'),
+        vjust=-.5,
+        hjust = 1,
+        color='grey',
+        family = 'Source Sans Pro',
+      size = 12),
+
+
+      
       # Legend
       legend.position = 'none',
       
       # panel grids
       panel.grid.minor.x = element_blank(),
       panel.grid.major.x = element_blank(), 
-      panel.grid.major.y = element_line(linewidth = .5),
+      panel.grid.major.y = element_line(linewidth = .4, color = 'grey'),
       panel.grid.minor.y = element_blank()
     )
 }
